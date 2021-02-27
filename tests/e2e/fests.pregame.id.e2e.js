74485ad4d70cd94506abdad632d35884c3141622
Jason Kennaly <jkennaly@gmail.com>
2021-02-27 11:14:39 -0800

    changed e2e tests from nightmare to puppeteer

--- tests/e2e/fests.pregame.id.e2e.js
+++ tests/e2e/fests.pregame.id.e2e.js
@@ -17,25 +17,25 @@ o.beforeEach(function() {
 
 o("FestiGram", function(done) {
 	o.timeout(30000)
-    browser
-      .then(b => b.newPage())
-      .then(p => p.goto(baseUrl, {waitUntil: 'networkidle0'}).then(r => p))
-      .then(p => p.waitForSelector(".ft-card").then(r => p))
-      .then(p => p.evaluate(() => {
-        return document.querySelector('.ft-stage-title').textContent
-        //done()
-      }))
-      .then(r => {
-        o('FestiGram Launcher').equals(r)
-      })
-      .catch(err => {
-      	o(err).equals('dead')
-      	//done()
-      })
-      .finally(() => {
-        browser.then((b) => b.close())
+  browser
+    .then(b => b.newPage())
+    .then(p => p.goto(baseUrl, {waitUntil: 'networkidle0'}).then(r => p))
+    .then(p => p.waitForSelector(".ft-card").then(r => p))
+    .then(p => p.evaluate(() => {
+      return document.querySelector('.ft-stage-title').textContent
+      //done()
+    }))
+    .then(r => {
+      o('FestiGram Launcher').equals(r)
+    })
+    .catch(err => {
+    	o(err).equals('dead')
+    	//done()
+    })
+    .finally(() => {
+      browser.then((b) => b.close())
 
-        done()
+      done()
 
-      })
+    })
 })
