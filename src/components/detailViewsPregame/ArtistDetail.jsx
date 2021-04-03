34cba33c99c842b5531ef7ec7a0e8d7abf93d0a5
Jason Kennaly <jkennaly@gmail.com>
2021-04-02 19:03:53 -0700

    improved component preloading

--- src/components/detailViewsPregame/ArtistDetail.jsx
+++ src/components/detailViewsPregame/ArtistDetail.jsx
@@ -1,28 +1,26 @@
 // src/components/detailViewsPregame/ArtistDetail.jsx
 
+import m from "mithril"
+import _ from "lodash"
+import { reviewSorter, festivalIdsByEndTimeSort } from "../../services/sorts"
 
-import m from 'mithril'
-import _ from 'lodash'
-import {reviewSorter, festivalIdsByEndTimeSort} from '../../services/sorts'
+import CardContainer from "../../components/layout/CardContainer.jsx"
+import FestivalCard from "../../components/cards/FestivalCard.jsx"
+import DateCard from "../../components/cards/DateCard.jsx"
+import NavCard from "../../components/cards/NavCard.jsx"
+import ReviewCard from "../../components/cards/ReviewCard.jsx"
+import SpotifyCard from "../../components/cards/SpotifyCard.jsx"
 
-import CardContainer from '../../components/layout/CardContainer.jsx';
-import FestivalCard from '../../components/cards/FestivalCard.jsx';
-import DateCard from '../../components/cards/DateCard.jsx';
-import NavCard from '../../components/cards/NavCard.jsx';
-import ReviewCard from '../../components/cards/ReviewCard.jsx';
-import SpotifyCard from '../../components/cards/SpotifyCard.jsx';
+import WidgetContainer from "../../components/layout/WidgetContainer.jsx"
+import FixedCardWidget from "../../components/widgets/FixedCard.jsx"
+import WikiWidget from "../../components/widgets/canned/WikiWidget.jsx"
+import DiscussionWidget from "../../components/widgets/canned/DiscussionWidget.jsx"
+import AdminWidget from "../../components/widgets/Admin.jsx"
+import UIButton from "../../components/ui/UIButton.jsx"
 
-import WidgetContainer from '../../components/layout/WidgetContainer.jsx';
-import FixedCardWidget from '../../components/widgets/FixedCard.jsx';
-import WikiWidget from '../../components/widgets/canned/WikiWidget.jsx';
-import DiscussionWidget from '../../components/widgets/canned/DiscussionWidget.jsx';
-import AdminWidget from '../../components/widgets/Admin.jsx';
-import UIButton from '../../components/ui/UIButton.jsx';
+import CloudImageField from "../../components/fields/CloudImageField.jsx"
 
-import CloudImageField from '../../components/fields/CloudImageField.jsx';
-
-
-import {remoteData} from '../../store/data';
+import { remoteData } from "../../store/data"
 
 const {
 	Artists: artists,
@@ -33,124 +31,164 @@ const {
 	Users: users
 } = remoteData
 
-
-
-const artist = (id) => artists.get(id)
-const comments = (id) => messages
-	.getFiltered({
-		subjectType: ARTIST, 
-		subject: id, 
-		messageType: COMMENT
-	})
-	.filter(m => users.get(m.fromuser))
+const artist = id => artists.get(id)
+const comments = id =>
+	messages
+		.getFiltered({
+			subjectType: ARTIST,
+			subject: id,
+			messageType: COMMENT
+		})
+		.filter(m => users.get(m.fromuser))
 
 const messageSorter = reviewSorter(interactions)
 
-const jsx = () => { 
+const jsx = () => {
 	let count = 5
 	return {
-	view: ({attrs}) => <div class="main-stage">
-
-			<WidgetContainer>
-			
-				<FixedCardWidget >
-					<CloudImageField 
-						userId={attrs.userId} 
-						userRoles={attrs.userRoles} 
-						subjectType={2} 
-						subject={attrs.artistId} 
-						sources={['url']} 
-						popModal={attrs.popModal}
-						addDisabled={!attrs.userRoles.includes('admin')}
-					/>
-				</FixedCardWidget>
-				{
-					//if this artist playing an active date and the user has an implicit checkin to the date, show a link to the artists sets in gametime
-					
-					
-				}
-				{attrs.wiki.length && attrs.wiki[0].length ? <WikiWidget text={attrs.wiki[0]} link={attrs.wiki[1]} /> : ''}
-				{attrs.artist ? <FixedCardWidget header="Listen & Review" >
-					<SpotifyCard fieldValue={attrs.artist.name} />
-					<ReviewCard type="artist" data={attrs.artist} popModal={attrs.popModal} />
-				</FixedCardWidget> : ''}
-				{_.isArray(attrs.userRoles) && attrs.userRoles.includes('admin') ? <AdminWidget header="Artist Admin">
-					<NavCard fieldValue="Fix Artist Names" action={() => m.route.set("/artists/pregame/fix/" + attrs.artistId)}/>
-				</AdminWidget> : ''}
-				<FixedCardWidget header="Festival Lineups">
-					{
-						remoteData.Lineups.festivalsForArtist(attrs.artistId)
-							.sort(festivalIdsByEndTimeSort(festivals))
-							.map(f => <FestivalCard eventId={f} artistId={attrs.artistId} />)
-					}
-				</FixedCardWidget>
-				{
-					//find each message about this attrs.artist and order by user
-					_.map(_.take(messageSorter(attrs.userId)(
-						comments(attrs.artistId)
-							), count)
-							,
-						me => <DiscussionWidget 
-							messageArray={[me]} 
+		view: ({ attrs }) => (
+			<div class="main-stage">
+				<WidgetContainer>
+					<FixedCardWidget>
+						<CloudImageField
 							userId={attrs.userId}
-							popModal={attrs.popModal} 
-							discussSubject={(so, me) => attrs.popModal('discuss', {
-								messageArray: me,
-								subjectObject: so,
-								reviewer: me[0].fromuser
-							})}
+							userRoles={attrs.userRoles}
+							subjectType={2}
+							subject={attrs.artistId}
+							sources={["url"]}
+							popModal={attrs.popModal}
+							addDisabled={!attrs.userRoles.includes("admin")}
 						/>
-					)
-				}
-				{ comments(attrs.artistId).length > count ? <UIButton action={e => {
-            //attrs.hide()
-            e.stopPropagation()
-            //console.log('pre rating ' + rating)
-            count += 3
-            m.redraw()
-        }} buttonName="Show More" /> : '' }
-			</WidgetContainer>
-		</div>
-}}
+					</FixedCardWidget>
+					{
+						//if this artist playing an active date and the user has an implicit checkin to the date, show a link to the artists sets in gametime
+					}
+					{attrs.wiki.length && attrs.wiki[0].length ? (
+						<WikiWidget text={attrs.wiki[0]} link={attrs.wiki[1]} />
+					) : (
+						""
+					)}
+					{attrs.artist ? (
+						<FixedCardWidget header="Listen & Review">
+							<SpotifyCard fieldValue={attrs.artist.name} />
+							<ReviewCard
+								type="artist"
+								data={attrs.artist}
+								popModal={attrs.popModal}
+							/>
+						</FixedCardWidget>
+					) : (
+						""
+					)}
+					{_.isArray(attrs.userRoles) && attrs.userRoles.includes("admin") ? (
+						<AdminWidget header="Artist Admin">
+							<NavCard
+								fieldValue="Fix Artist Names"
+								action={() =>
+									m.route.set("/artists/pregame/fix/" + attrs.artistId)
+								}
+							/>
+						</AdminWidget>
+					) : (
+						""
+					)}
+					<FixedCardWidget header="Festival Lineups">
+						{remoteData.Lineups.festivalsForArtist(attrs.artistId)
+							.sort(festivalIdsByEndTimeSort(festivals))
+							.map(f => (
+								<FestivalCard eventId={f} artistId={attrs.artistId} />
+							))}
+					</FixedCardWidget>
+					{//find each message about this attrs.artist and order by user
+					_.map(
+						_.take(
+							messageSorter(attrs.userId)(comments(attrs.artistId)),
+							count
+						),
+						me => (
+							<DiscussionWidget
+								messageArray={[me]}
+								userId={attrs.userId}
+								popModal={attrs.popModal}
+								discussSubject={(so, me) =>
+									attrs.popModal("discuss", {
+										messageArray: me,
+										subjectObject: so,
+										reviewer: me[0].fromuser
+									})
+								}
+							/>
+						)
+					)}
+					{comments(attrs.artistId).length > count ? (
+						<UIButton
+							action={e => {
+								//attrs.hide()
+								e.stopPropagation()
+								//console.log('pre rating ' + rating)
+								count += 3
+								m.redraw()
+							}}
+							buttonName="Show More"
+						/>
+					) : (
+						""
+					)}
+				</WidgetContainer>
+			</div>
+		)
+	}
+}
 
 const ArtistDetail = {
-		preload: (rParams) => {
-			//if a promise returned, instantiation of component held for completion
-			//route may not be resolved; use rParams and not m.route.param
-			const artistId = parseInt(rParams.id, 10)
-			messagesMonitors.remoteCheck(true)
-			//messages.forArtist(artistId)
-			//console.log('Artist Detail preload')
-			return artists.subjectDetails({subject: artistId, subjectType: ARTIST})
-				.then(m.redraw)
-		},
-	oncreate: ({dom}) => {
+	preload: rParams => {
+		//if a promise returned, instantiation of component held for completion
+		//route may not be resolved; use rParams and not m.route.param
+		const artistId = parseInt(rParams.id, 10)
+
+		//messages.forArtist(artistId)
+		//console.log("Artist Detail preload", artistId)
+		return Promise.all([
+			artists
+				.subjectDetails({ subject: artistId, subjectType: ARTIST })
+				.then(() => {
+					const artist = artists.get(artistId)
+					if (artist) rParams.titleSet(artist.name)
+					//console.log("ArtistDetail preload complete", artist.name)
+					return
+				}),
+			messagesMonitors.remoteCheck(true),
+			artists.getWikiPromise(artistId)
+		])
+			.then(m.redraw)
+			.catch(console.error)
+	},
+	oncreate: ({ dom }) => {
 		const height = dom.clientHeight
 		//console.log('ArtistDetail DOM height', height)
-		const scroller = dom.querySelector('.ft-widget-container')
-		scroller.style['height'] = `${height - 270}px`
-		scroller.style['flex-grow'] = 0
-
+		const scroller = dom.querySelector(".ft-widget-container")
+		scroller.style["height"] = `${height - 270}px`
+		scroller.style["flex-grow"] = 0
 	},
-		view: ({attrs}) => {
-			const artistId = parseInt(m.route.param('id'), 10)
-			const artist = artists.get(artistId)
-			if(artist) attrs.titleSet(artist.name)
-			/*
+	view: ({ attrs }) => {
+		const artistId = parseInt(m.route.param("id"), 10)
+		const artist = artists.get(artistId)
+		if (artist) attrs.titleSet(artist.name)
+		/*
 			const activeDateSets = remoteData.Sets.getFiltered({band: attrs.artistId})
 				.filter(s => remoteData.Dates.active(remoteData.Days.getDateId(s.day)))
 				*/
-			//console.log('ArtistDetail view', parseInt(m.route.param('id'), 10), artists.get(parseInt(m.route.param('id'), 10)))
-			const mapping = {
-				userId: attrs.userId,
-				userRoles: attrs.userRoles,
-				popModal: attrs.popModal,
-				artistId: artistId,
-				artist: artist,
-				wiki: artists.getWiki(artistId)
-				//sets: activeDateSets
-			}
-			return m(jsx, mapping)
+		//console.log('ArtistDetail view', parseInt(m.route.param('id'), 10), artists.get(parseInt(m.route.param('id'), 10)))
+		const mapping = {
+			userId: attrs.userId,
+			userRoles: attrs.userRoles,
+			popModal: attrs.popModal,
+			artistId: artistId,
+			artist: artist,
+			wiki: artists.getWiki(artistId)
+			//sets: activeDateSets
 		}
+		return m(jsx, mapping)
+	}
 }
-export default ArtistDetail;
+export default ArtistDetail
--- src/components/detailViewsPregame/FestivalDetail.jsx
+++ src/components/detailViewsPregame/FestivalDetail.jsx
@@ -1,118 +1,123 @@
 // src/components/detailViewsPregame/FestivalDetail.jsx
 
-import m from "mithril";
-import _ from 'lodash';
+import m from "mithril"
+import _ from "lodash"
 
-import CardContainer from "../../components/layout/CardContainer.jsx";
-import SeriesCard from "../../components/cards/SeriesCard.jsx";
-import DateCard from "../../components/cards/DateCard.jsx";
-import NavCard from "../../components/cards/NavCard.jsx";
-import ArtistCard from "../../components/cards/ArtistCard.jsx";
+import CardContainer from "../../components/layout/CardContainer.jsx"
+import SeriesCard from "../../components/cards/SeriesCard.jsx"
+import DateCard from "../../components/cards/DateCard.jsx"
+import NavCard from "../../components/cards/NavCard.jsx"
+import ArtistCard from "../../components/cards/ArtistCard.jsx"
 
-import WidgetContainer from "../../components/layout/WidgetContainer.jsx";
-import FixedCardWidget from "../../components/widgets/FixedCard.jsx";
-import ResearchWidget from "../../components/widgets/canned/ResearchWidget.jsx";
-import LineupWidget from "../../components/widgets/canned/LineupWidget.jsx";
-import ActivityWidget from "../../components/widgets/canned/ActivityWidget.jsx";
-import IntentToggle from "../../components/ui/canned/IntentToggle.jsx";
+import WidgetContainer from "../../components/layout/WidgetContainer.jsx"
+import FixedCardWidget from "../../components/widgets/FixedCard.jsx"
+import ResearchWidget from "../../components/widgets/canned/ResearchWidget.jsx"
+import LineupWidget from "../../components/widgets/canned/LineupWidget.jsx"
+import ActivityWidget from "../../components/widgets/canned/ActivityWidget.jsx"
+import IntentToggle from "../../components/ui/canned/IntentToggle.jsx"
 
-import SeriesWebsiteField from './fields/series/SeriesWebsiteField.jsx'
+import SeriesWebsiteField from "./fields/series/SeriesWebsiteField.jsx"
 
-import { remoteData } from "../../store/data";
+import { remoteData } from "../../store/data"
 
 const festivals = remoteData.Festivals
 
-const jsx = () => { 
+const jsx = () => {
 	return {
-		view: ({attrs}) => <div class="main-stage">
-			{attrs.festivalId ? <SeriesWebsiteField festivalId={attrs.festivalId} /> : ''}
-			{attrs.festivalId ? (
-				<IntentToggle
-					subjectObject={{
-						subject: attrs.festivalId, 
-						subjectType: FESTIVAL
-					}}
-					permission={attrs.userRoles.includes('user')}
-				/>
-			) : (
-				""
-			)}
-
-			<WidgetContainer>
-				<LineupWidget festivalId={attrs.festivalId} />
-				<ActivityWidget 
-					festivalId={attrs.festivalId} 
-					userId={attrs.userId} 
-					popModal={attrs.popModal} 
-				/>
-				<ResearchWidget list={[]} userId={attrs.userId} popModal={attrs.popModal} />
-				<FixedCardWidget header="Related Events">
-					{(remoteData.Dates.getMany(
-						festivals.getSubIds(attrs.festivalId))
-					)
-						.sort((a, b) => a.basedate - b.basedate)
-						.map(data => (
-							<DateCard eventId={data.id} />
-						))}
-					<SeriesCard
-						data={(remoteData.Series.get(
-							festivals.getSuperId(
-								attrs.festivalId))
-						)}
-						eventId={(
-							festivals.getSuperId(
-								attrs.festivalId)
-						)}
+		view: ({ attrs }) => (
+			<div class="main-stage">
+				{attrs.festivalId ? (
+					<SeriesWebsiteField festivalId={attrs.festivalId} />
+				) : (
+					""
+				)}
+				{attrs.festivalId ? (
+					<IntentToggle
+						subjectObject={{
+							subject: attrs.festivalId,
+							subjectType: FESTIVAL
+						}}
+						permission={attrs.userRoles.includes("user")}
 					/>
-				</FixedCardWidget>
-			</WidgetContainer>
-		</div>
-	}}
+				) : (
+					""
+				)}
+
+				<WidgetContainer>
+					<LineupWidget festivalId={attrs.festivalId} />
+					<ActivityWidget
+						festivalId={attrs.festivalId}
+						userId={attrs.userId}
+						popModal={attrs.popModal}
+					/>
+					<ResearchWidget
+						list={[]}
+						userId={attrs.userId}
+						popModal={attrs.popModal}
+					/>
+					<FixedCardWidget header="Related Events">
+						{remoteData.Dates.getMany(festivals.getSubIds(attrs.festivalId))
+							.sort((a, b) => a.basedate - b.basedate)
+							.map(data => (
+								<DateCard eventId={data.id} />
+							))}
+						<SeriesCard
+							data={remoteData.Series.get(
+								festivals.getSuperId(attrs.festivalId)
+							)}
+							eventId={festivals.getSuperId(attrs.festivalId)}
+						/>
+					</FixedCardWidget>
+				</WidgetContainer>
+			</div>
+		)
+	}
+}
 const FestivalDetail = {
-	name: 'FestivalDetail',
-	preload: (rParams) => {
+	name: "FestivalDetail",
+	preload: rParams => {
 		//if a promise returned, instantiation of component held for completion
 		//route may not be resolved; use rParams and not m.route.param
 		const festivalId = parseInt(rParams.id, 10)
 		//messages.forArtist(festivalId)
 		///console.log('FestivalDetail preload', festivalId, rParams)
-		if(festivalId) return festivals.subjectDetails({subject: festivalId, subjectType: FESTIVAL})
-			.catch(err => console.error('FestivalDetail load incomplete'))
+		return festivals
+			.subjectDetails({ subject: festivalId, subjectType: FESTIVAL })
+			.then(() => rParams.titleSet(festivals.getEventName(festivalId)))
 			.then(m.redraw)
+			.catch(err => console.error("FestivalDetail load incomplete"))
 	},
-	oncreate: ({dom}) => {
+	oncreate: ({ dom }) => {
 		const height = dom.clientHeight
 		//console.log('ArtistDetail DOM height', height)
-		const scroller = dom.querySelector('.ft-widget-container')
-		scroller.style['height'] = `${height - 270}px`
-		scroller.style['flex-grow'] = 0
-
+		const scroller = dom.querySelector(".ft-widget-container")
+		scroller.style["height"] = `${height - 270}px`
+		scroller.style["flex-grow"] = 0
 	},
-		view: ({attrs}) => {
-			const festivalId = parseInt(m.route.param('id'), 10)
-			attrs.titleSet(festivals.getEventName(festivalId))
-			const so = {subjectType: FESTIVAL, subject: festivalId}
-			attrs.focusSubject(so)
-			const decoded = attrs.auth.gtt()
-			const accessible = decoded && !festivals.sellAccess(festivalId, decoded)
-			const evtString = accessible ? 'hasAccess' : 'noAccess'
-			attrs.eventSet(evtString)
-			//console.log('FestivalDetail ciew evtString, decoded', evtString, decoded)
-			//hasAccess if:
-				//event not restricted OR
-				//gtt token has access
-			//console.log('FestivalDetail', attrs.gtt)
-			
-			const mapping = {
-				userId: attrs.userId,
-				userRoles: attrs.userRoles,
-				popModal: attrs.popModal,
-				festivalId: festivalId,
+	view: ({ attrs }) => {
+		const festivalId = parseInt(m.route.param("id"), 10)
 
-				//sets: activeDateSets
-			}
-			return m(jsx, mapping)
+		const so = { subjectType: FESTIVAL, subject: festivalId }
+		attrs.focusSubject(so)
+		const decoded = attrs.auth.gtt()
+		const accessible = decoded && !festivals.sellAccess(festivalId, decoded)
+		const evtString = accessible ? "hasAccess" : "noAccess"
+		attrs.eventSet(evtString)
+		//console.log('FestivalDetail ciew evtString, decoded', evtString, decoded)
+		//hasAccess if:
+		//event not restricted OR
+		//gtt token has access
+		//console.log('FestivalDetail', attrs.gtt)
+
+		const mapping = {
+			userId: attrs.userId,
+			userRoles: attrs.userRoles,
+			popModal: attrs.popModal,
+			festivalId: festivalId
+
+			//sets: activeDateSets
 		}
-
-};
-export default FestivalDetail;
+		return m(jsx, mapping)
+	}
+}
+export default FestivalDetail
